066f510502f66a1a4f4ca2de85a00a56bbfe724d
q.thompson <q.thompson088@gmail.com>
2022-11-09 12:44:12 -0800

    Sends an alert if the inputted course number is on the course list

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 let counter = 0;
 let userInput;
-let courses = [1620, 1116, 1515];
+let courses = {1620:'ACIT', 1116:'COMM', 1515:'ACIT'};
 
 do {
     if (counter === 0) {
@@ -10,3 +10,9 @@ do {
     }
     counter += 1
 } while (isNaN(Number(userInput)) || userInput < 1000);
+
+if (courses[userInput] !== undefined) {
+    alert(`${courses[userInput]}-${userInput} is on the schedule.`)
+} else {
+    alert(`${userInput} is not on the schedule.`)
+}
